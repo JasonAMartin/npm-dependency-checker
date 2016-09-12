08a7004ea096459262d0dcfe9f452d302751c007
Jason A. Martin <jason.martin@jasonamartin.com>
2016-09-11 18:42:31 -0700

    feat: adding HTTPoison and Poison dependencies.

--- ndc/mix.exs
+++ ndc/mix.exs
@@ -15,7 +15,7 @@ defmodule Ndc.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison, :poison]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +28,9 @@ defmodule Ndc.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:poison, "~> 2.0"},
+      {:httpoison, "~> 0.9.0"}
+    ]
   end
 end
