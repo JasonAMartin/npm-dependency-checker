eb299de5148dd4beb08eca95be0124ac3327dbef
Jason A. Martin <jason.martin@jasonamartin.com>
2016-09-12 20:10:57 -0700

    refactor: cleaning up tests

--- ndc/test/ndc_test.exs
+++ ndc/test/ndc_test.exs
@@ -18,20 +18,6 @@ defmodule NdcTest do
     refute Ndc.npm_view(1337)
   end
 
-  test "get_package_repo_url: returns a url when used in conjunction with npm_view" do
-    assert Ndc.get_package_repo_url(Ndc.npm_view("microlibrary")) == "https://github.com/JasonAMartin/microlibrary#readme"
-  end
-
-  test "fetch_package_json(repo): prints msg when it gets a 404 error" do
-    assert Ndc.fetch_package_json("http://www.elixirdev.io/23424234234ewfewwee/") == "Your princess is in another castle."
-  end
-
-  test "fetch_package_json(repo): raises UndefinedFunctionError when passing in a non-url string " do
-    assert_raise UndefinedFunctionError, fn ->
-      Ndc.fetch_package_json("cincinnati_bengals")
-    end
-  end
-
   test "fetch_package_json(repo): raises UndefinedFunctionError when passing in numbers" do
     assert_raise UndefinedFunctionError, fn ->
       Ndc.fetch_package_json(3434234)
@@ -39,14 +25,15 @@ defmodule NdcTest do
   end
 
   test "transform_repo_raw_json_url(repo): returns the proper github raw source url" do
-    assert Ndc.transform_repo_raw_json_url("https://github.com/JasonAMartin/node-dependency-checker") == "https://raw.githubusercontent.com/JasonAMartin/node-dependency-checker/master/package.json"
+    assert Ndc.transform_repo_raw_json_url([repository: %{"type" => "git",
+   "url" => "git+https://github.com/JasonAMartin/npm-dependency-checker.git"}]) == "https://raw.githubusercontent.com/JasonAMartin/npm-dependency-checker/master/package.json"
   end
 
-  test "transform_repo_raw_json_url(repo): returns false when a non-binary is passed in" do
+  test "transform_repo_raw_json_url(repo): returns false when a non-list is passed in" do
     refute Ndc.transform_repo_raw_json_url(3)
   end
 
   test "decode_body: returns map" do
-    assert Ndc.decode_body("{\n  \"name\": \"microlibrary\",\n  \"version\": \"1.2.6\",\n \"repository\": {\n  \"type\": \"git\",\n \"url\": \"git+https://github.com/JasonAMartin/microlibrary.git\"\n  },\n  \"dependencies\": {\n    \"unique-random-array\": \"1.0.0\"\n  },\n  \"devDependencies\": {\n    \"babel\": \"^6.1.18\",\n   \"mocha-lcov-reporter\": \"^1.0.0\"\n  }\n}\n")
+    assert Ndc.decode_body("{\n  \"name\": \"microlibrary\",\n  \"version\": \"1.2.6\",\n \"repository\": {\n  \"type\": \"git\",\n \"url\": \"git+https://github.com/JasonAMartin/microlibrary.git\"\n  },\n  \"dependencies\": {\n    \"unique-random-array\": \"1.0.0\"\n  },\n  \"devDependencies\": {\n    \"babel\": \"^6.1.18\",\n   \"mocha-lcov-reporter\": \"^1.0.0\"\n  }\n}\n", ~w(devDependencies dependencies ))
   end
 end
