03ba0608e618672e508de076ff653e5e37ebd31f
Jason A. Martin <jason.martin@jasonamartin.com>
2016-09-12 18:45:33 -0700

    refactor: adding doc information and function guards.

--- ndc/lib/ndc.ex
+++ ndc/lib/ndc.ex
@@ -31,28 +31,52 @@ defmodule Ndc do
       |> (fn x -> iterate_dependencies(elem(x, 1)) end).()
   end
 
-  def iterate_dependencies(map) do
+  @doc """
+  Iterates through a map and prints out the key:value pairs
+  Returns `:ok`.
+  """
+  def iterate_dependencies(map) when is_map(map) do
     ## More work to do.
     Enum.map(map, fn {k, v} -> IO.inspect [k,v] end)
   end
 
-  def npm_view(package) do
+  @doc """
+  Returns false when a non-map argument is passed in
+  """
+  def iterate_dependencies(_anything_else), do: false
+
+  @doc """
+  Takes a String and runs the system command "npm view <package>"
+  Returns: the output of that command
+  """
+  def npm_view(package) when is_binary(package) do
     System.cmd("npm", ["view", package])
   end
 
+  @doc """
+  Handles all non-string arguments and returns false since we're not doing anything with it
+  """
+  def npm_view(_anything), do: false
+
+  @doc """
+  Runs a regex on data passed in and returns data between the start and stop.
+  """
   def get_package_repo_url(data) do
     ## sending output from npm view on the command line.
      Regex.run(~r/homepage: \'(.*)\'/, elem(data,0)) |> Enum.at(1)
   end
 
+  @doc """
+  Fetches a url, which should be a JSON file and sends it to decode_body for decoding to a map
+  """
   def fetch_package_json(repo) do
     case HTTPoison.get(repo) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         decode_body(body)
       {:ok, %HTTPoison.Response{status_code: 404}} ->
-        IO.puts "Not found :("
+        "Your princess is in another castle."
       {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect reason
+        raise reason
     end
   end
 
@@ -63,13 +87,22 @@ defmodule Ndc do
     |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
   end
 
-  def transform_repo_raw_json_url(repo) do
+
+  @doc """
+  Takes a GitHub url and transforms it to point to the package.json in the master branch
+  """
+  def transform_repo_raw_json_url(repo) when is_binary(repo) do
     ## we will do a little replace therapy to fetch the raw json.
     ## end result is: https://raw.githubusercontent.com/someuser/somerepo/master/package.json
     String.replace(repo, "#readme","")
       |> String.replace("github","raw.githubusercontent")
       |>  (fn x -> x <> "/master/package.json" end).()
   end
+
+  @doc """
+  Returns false when a non-binary is passed in
+  """
+  def transform_repo_raw_json_url(_anything_else), do: false
 
   # Handle parsing of arguments
   defp parse_args(args) do
